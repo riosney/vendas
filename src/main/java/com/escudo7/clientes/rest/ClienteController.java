db44f86da85180d1c12571f1a9f725277c2182ed
Riosney <rsantosdo@minsait.com>
2022-09-27 11:51:04 -0300

    Criando controller e método salvar

---
+++ src/main/java/com/escudo7/clientes/rest/ClienteController.java
@@ -0,0 +1,31 @@
+package com.escudo7.clientes.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.escudo7.clientes.model.entity.Cliente;
+import com.escudo7.clientes.model.repository.ClienteRepository;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+	
+	private final ClienteRepository repository;
+	
+	@Autowired
+	public ClienteController(ClienteRepository repository) {
+		this.repository = repository;
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente salvar(Cliente cliente) {
+		return repository.save(cliente);
+	}
+	
+
+}
