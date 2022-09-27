dd89a0218230008a55aa37a0a84fc75ba6dafb11
Riosney <rsantosdo@minsait.com>
2022-09-27 14:32:47 -0300

    Atualizar cliente

--- src/main/java/com/escudo7/clientes/rest/ClienteController.java
+++ src/main/java/com/escudo7/clientes/rest/ClienteController.java
@@ -2,9 +2,11 @@ package com.escudo7.clientes.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -36,6 +38,31 @@ public class ClienteController {
 		return repository
 				.findById(id)
 				.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND) );
+	}
+	
+	@DeleteMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar(@PathVariable Integer id) {
+		repository
+			.findById(id)
+			.map(cliente -> {
+				repository.delete(cliente);
+				return Void.TYPE;
+			})
+			.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND) );
+	}
+	
+	@PutMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void atualizar(@PathVariable Integer id, @RequestBody Cliente clienteAtualizado) {
+		repository
+		.findById(id)
+		.map(cliente -> {
+			cliente.setNome(clienteAtualizado.getNome());
+			cliente.setCpf(clienteAtualizado.getCpf());
+			return repository.save(clienteAtualizado);
+		})
+		.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND) );
 	}
 
 }
