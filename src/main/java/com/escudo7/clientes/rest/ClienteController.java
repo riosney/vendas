936db19e9a85455076f2f6b7ba0aa624a54a2057
Riosney <rsantosdo@minsait.com>
2022-10-11 16:08:27 -0300

    Criando CrossOrigin

--- src/main/java/com/escudo7/clientes/rest/ClienteController.java
+++ src/main/java/com/escudo7/clientes/rest/ClienteController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.escudo7.clientes.model.repository.ClienteRepository;
 
 @RestController
 @RequestMapping("/api/clientes")
+@CrossOrigin("http://localhost:4200")
 public class ClienteController {
 	
 	private final ClienteRepository repository;
